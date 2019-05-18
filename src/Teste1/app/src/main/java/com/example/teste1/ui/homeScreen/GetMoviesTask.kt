fcf24096c06e5bf34c73cbd2f042df45b488ea19
gabrielbmoro <gabrielbronzattimoro.es@gmail.com>
2019-05-18 15:22:52 -0300

    Mudando para chamada síncrona

--- src/Teste1/app/src/main/java/com/example/teste1/ui/homeScreen/GetMoviesTask.kt
+++ src/Teste1/app/src/main/java/com/example/teste1/ui/homeScreen/GetMoviesTask.kt
@@ -1,13 +1,8 @@
 package com.example.teste1.ui.homeScreen
 
 import android.os.AsyncTask
-import android.util.Log
 import com.example.teste1.api.RetrofitHandler
-import com.example.teste1.model.GetMoviesResponse
 import com.example.teste1.model.Movie
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class GetMoviesTask(contractArgument: UpdateHomeActivity) : AsyncTask<String?, List<Movie>?, List<Movie>?>() {
 
@@ -18,22 +13,10 @@ class GetMoviesTask(contractArgument: UpdateHomeActivity) : AsyncTask<String?, L
 
     override fun doInBackground(vararg params: String?): List<Movie>? {
 
-        RetrofitHandler.repository.getMovies().enqueue(object : Callback<List<GetMoviesResponse>> {
-
-            override fun onFailure(call: Call<List<GetMoviesResponse>>, t: Throwable) {
-                list = emptyList()
-                Log.e("onFailure", t.message)
-            }
-
-            override fun onResponse(call: Call<List<GetMoviesResponse>>, response: Response<List<GetMoviesResponse>>) {
-                list = response.body()?.firstOrNull()?.moviesList ?: emptyList()
-                list.toString()
-            }
-
-        })
-
-        while (list == null)
-            Thread.sleep(1000)
+        list = RetrofitHandler.repository
+            .getMovies()
+            .execute()
+            .body()?.firstOrNull()?.moviesList
 
         return list
     }
